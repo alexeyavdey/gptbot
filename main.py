c3e57224ca2742c25172e7799570ea605d1b3586
alexeyavdey <alexey.avdey@gmail.com>
2025-06-07 11:04:40 +0300

    Update bot defaults and docs

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 from .handlers import router
 from . import env
 
 
 async def main():
-  bot = Bot(token=env.BOT_TOKEN, parse_mode=ParseMode.MARKDOWN)
+  bot = Bot(
+      token=env.BOT_TOKEN,
+      default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN),
+  )
 
   dp = Dispatcher()
   dp.include_router(router)
